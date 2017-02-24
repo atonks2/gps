80267d0dad605191c5700b165064ba63c32fc29b
atonks2 <atonks2@gmail.com>
2017-02-24 16:15:19 -0700

    Fixed mistake in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include "gps.h"
+#include "gpstypes.h"
 
 int main()
 {
 	GPS gps(4800, "/dev/ttyUSB0");
 
-	gps.updateGPGGA();
-	gps.printGPGGA();
+	GPGGA data{ gps.updateGPGGA() };
+	gps.printGPGGA(data);
 
 	return 0;
 }
