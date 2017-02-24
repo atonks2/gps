b5ec84a05ec74ab99b72db5b89103d4e06a3d735
atonks2 <atonks2@gmail.com>
2017-02-24 16:27:18 -0700

    added test for bearing() and distance() to main()

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,19 @@ int main()
 {
 	GPS gps(4800, "/dev/ttyUSB0");
 
+	std::cout << "Reading from GPS...";
 	GPGGA data = gps.updateGPGGA();
+	std::cout << "done.\n";
 	gps.printGPGGA(data);
+
+	// lat long of known location (the Whitehouse)
+	double latitude = 38.8977;
+	double longitude = -77.0365;
+
+	std::cout << "Distance between current location and the White House: "
+		<< gps.distance(data.latitude, data.longitude, latitude, longitude) << std::endl;
+	std::cout << "Bearing from current location to the White House: "
+		<< gps.bearing(data.latitude, data.longitude, latitude, longitude) << std::endl;
 
 	return 0;
 }
